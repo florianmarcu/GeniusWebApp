51880f2e04ad62cf6147590d1c65cd8247c8b6f1
Dawlau <blahoviciandrei1gmail.com>
2021-01-04 12:24:35 +0200

    This script unchecks a box if the other one is checked

---
+++ GeniusWebApp/Scripts/CheckBoxes.js
@@ -0,0 +1,20 @@
+﻿window.onload = function () {
+
+    var checkboxes = document.getElementsByName("Visibility");
+    var checkbox1 = checkboxes[0];
+    var checkbox2 = checkboxes[1];
+
+    console.log(checkbox1, checkbox2);
+
+    checkbox1.onclick = function () {
+
+        checkbox1.checked = true;
+        checkbox2.checked = false;
+    }
+
+    checkbox2.onclick = function () {
+
+        checkbox1.checked = false;
+        checkbox2.checked = true;
+    }
+}
