eeded68d32fbe421f1a29e735b2e2b34fd089ae4
Dawlau <blahoviciandrei1gmail.com>
2021-01-07 00:37:06 +0200

    User can make new posts

---
+++ GeniusWebApp/Controllers/UserPostController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeniusWebApp.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GeniusWebApp.Controllers
+{
+    public class UserPostController : Controller
+    {
+        ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: UserPost
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(UserPost post)
+        {
+            var userId = User.Identity.GetUserId();
+            var userProfile = (from userprofile in _db.UserProfiles
+                               where userprofile.User.Id == userId
+                               select userprofile).First<UserProfile>();
+
+            post.Profile = userProfile;
+            System.Diagnostics.Debug.WriteLine(userProfile);
+
+            _db.UserPosts.Add(post);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Manage");
+        }
+
+
+        public ActionResult Show(int UserPostId)
+        {
+            var posts = _db.UserPosts.Find(UserPostId);
+
+
+            return View();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int GeniusUserProfileId)
+        {
+            return View();
+        }
+
+        [HttpPut]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
