85c329666e39513dfa1d58cae34625e1f4a9f195
Dawlau <blahoviciandrei1gmail.com>
2021-08-29 15:53:31 +0300

    backup

---
+++ GeniusWebApp/Controllers/AdminMessagesController.cs
@@ -0,0 +1,47 @@
+﻿using GeniusWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeniusWebApp.Controllers
+{
+    public class AdminMessagesController : Controller
+    {
+        ApplicationDbContext _db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(string message, string userId)
+        {
+            AdminMessage adminMessage = new AdminMessage();
+            adminMessage.message = message;
+            adminMessage.UserId = userId;
+
+            _db.AdminMessages.Add(adminMessage);
+            _db.SaveChanges();
+
+            ViewBag.firstName = TempData["firstName"].ToString();
+            ViewBag.lastName = TempData["lastName"].ToString();
+
+            return RedirectToAction("ShowAll", "UserProfile", new { firstName = TempData["firstName"].ToString(), lastName = TempData["lastName"].ToString() });
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int Id)
+        {
+            System.Diagnostics.Debug.WriteLine(Id);
+
+            var adminMessage = _db.AdminMessages.Find(Id);
+            _db.AdminMessages.Remove(adminMessage);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ GeniusWebApp/Models/AdminMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeniusWebApp.Models
+{
+    public class AdminMessage
+    {
+        [Key]
+        public int AdminMessageId { get; set; }
+        public string message { get; set; }
+        public string UserId { get; set; }
+    }
+}
